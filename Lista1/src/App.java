fb31a3203a5aec87c59b4e7599d201b2b9e6825b
Gustavo <gustavo.lent@icarotech.com>
2022-05-15 21:05:58 -0300

    Teste

--- Lista1/src/App.java
+++ Lista1/src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner scanner = new Scanner(System.in);
         // Exercicio1 exercicio1 = new Exercicio1();
-        // exercicio1.run(scanner);
+        // exercicio1.run(scanner); 
 
         // Exercicio2 exercicio2 = new Exercicio2();
         // exercicio2.run(scanner);
